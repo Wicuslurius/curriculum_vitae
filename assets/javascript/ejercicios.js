61926142245f4b9b12587958c056e7fd6464394d
luis miguel aranguren ardila <miguelo.5@hotmail.com>
2025-01-17 10:03:40 -0500

    en este commit guardamos ejercicios de javascript

---
+++ assets/javascript/ejercicios.js
@@ -0,0 +1,13 @@
+//--------------------------------------------------------------------->
+
+/*const hora = new Date().getHours();
+let mensaje;
+if(hora < 13) mensaje = 'buenos dias';
+else if (hora < 21) mensaje = 'buenas tardes';
+else if (hora < 24) mensaje = 'buenas noches';
+document.getElementById('output').innerHTML = mensaje;*/
+
+//---------------------------------------------------------------------->
+
+/*const saludo = 'hola mundo';
+document.getElementById('output').innerHTML = saludo;*/
