070b715c5afa4144c064cda8ce6fa32a1e9fa979
luis miguel aranguren ardila <miguelo.5@hotmail.com>
2025-01-23 14:15:14 -0500

    ejercicio de js

--- assets/javascript/ejercicios.js
+++ assets/javascript/ejercicios.js
@@ -1,15 +1,27 @@
 alert('hola mundo desde el js de ejercicios');
 
+
+//--------------------------------------------------------------->
+//ejercicio seis
+/*const dia = new Date().getDay();
+
 function esFinDeSemana() {
-  const dia = new Date().getDate();
-  if(dia === 0 || dia === 6)return true;
-  else return false;
+  return dia === 0 || dia === 6;
 }
-let valorSrc;
-if (esFinDeSemana() === true) valorSrc = './../assets/img/img_exercise/CaritaFeliz.png';
-else valorSrc = './../assets/img/img_exercise/CaritaTriste.png';
+
+function queDia() {
+  const nombreDia = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+  return nombreDia[dia];
+}
+//let valorSrc;
+//if (esFinDeSemana() === true) valorSrc = './../assets/img/img_exercise/CaritaFeliz.png';
+//else valorSrc = './../assets/img/img_exercise/CaritaTriste.png';
+const valorSrc = esFinDeSemana() ? './../assets/img/img_exercise/CaritaFeliz.png' : './../assets/img/img_exercise/CaritaTriste.png';
+const mensaje = esFinDeSemana() ?  "Diviertete" : "Venga, que ya falta poco";
 
 document.getElementById('carita').src = valorSrc;
+document.getElementById("output").innerHTML = mensaje;
+document.getElementById("day").innerHTML = queDia();*/
 
 //--------------------------------------------------------------->
 //ejercicio cinco
