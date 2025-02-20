7a02c434bad1e3ee297c69d493614a7c946c27cf
luis miguel aranguren ardila <miguelo.5@hotmail.com>
2025-02-20 11:14:03 -0500

    en este commit trabajamos en ejercicios de javascript pero el ultimo el de la pagina 53 no funciona , necesitamos revisarlo

--- assets/javascript/ejercicios.js
+++ assets/javascript/ejercicios.js
@@ -1,6 +1,31 @@
 alert('hola mundo desde el js de ejercicios');
 
+function cuantosDias(anio, mes, dia, nombreEvento){
+  const hoy = new Date();
+  const milsegEnUnDia = 1000 * 60 * 60 * 24; // milisegundos de un dia
+  hoy.setHours(0,0,0,0);
+  const fechaEvento = new Date(anio, mes - 1 , dia);
+  const diferenciaHoras = Math.abs(fechaEvento.getTimezoneOffset() / 60) - Math.abs(hoy.getTimezoneOffset / 60);
+  hoy.setHours(hoy.getHours() - diferenciaHoras);
 
+  if (fechaEvento > hoy) {
+    return `Faltan ${(fechaEvento.getTime() - hoy.getTime()) / milsegEnUnDia} dias para ${nombreEvento}`;
+  } else {
+    return 'la fecha ya paso';
+  }
+}
+
+document.getElementById('output').innerHTML = cuantosDias(2026, 12, 25, 'Navidad');
+//--------------------------------------------------------------->
+/*
+function queDia(nombre){
+  const dia = new Date().getDay();
+  const nombreDia =  ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+  return `hola ${nombre} Feliz ${nombreDia[dia]}`;
+}
+
+document.getElementById('output').innerHTML = queDia('Alessandra');
+*/
 //--------------------------------------------------------------->
 //ejercicio seis
 /*const dia = new Date().getDay();
